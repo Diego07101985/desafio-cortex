3942d707c433e042e41e0f0603c1b83f4a00e2d0
Diego Salles <disalles222@hotmail.com>
2021-01-08 13:07:50 -0200

    ajuste sonar

--- desafio/currency/models.py
+++ desafio/currency/models.py
@@ -9,10 +9,3 @@ class RequestCurrencyQuotationParam():
         self.initial_date = initial_date
         self.final_date = final_date
 
-@dataclass
-class RequestCurrencyQuotationParam():
-    def __init__(self, from_simbol, to_simbol, initial_date, final_date):
-        self.to_simbol = to_simbol
-        self.from_simbol = from_simbol
-        self.initial_date = initial_date
-        self.final_date = final_date
